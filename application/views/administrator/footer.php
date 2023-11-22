483e6b85b17d4ca59980ffb0c33d152c631a34d1
Andy Ismail <98675756+simunda@users.noreply.github.com>
2023-11-22 08:48:19 +0700

    ok

---
+++ application/views/administrator/footer.php
@@ -0,0 +1,2 @@
+
+<strong>Copyright &copy; 2023 - <?php echo date('Y'); ?> <a target='_BLANK' href="https://mentaya.shop"> Mentaya Online Marketplace</a>.</strong> All rights reserved. 
---
+++ application/views/reseller/footer.php
@@ -0,0 +1,2 @@
+
+<strong>Copyright &copy; 2023 - <?php echo date('Y'); ?> <a target='_BLANK' href="https://mentaya.shop"> Mentaya Online Marketplace - Dashboard Pelapak</a>.</strong> All rights reserved. 
